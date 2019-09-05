03bc12ac3d40dfe3692e703ed8ff4a3b8c09b2c0
Connor McDermott <connor.mcdermott@perficient.com>
2019-09-05 17:02:19 -0500

    Basic command line interpreting started

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace GetIgnore
 {
@@ -6,7 +11,24 @@ namespace GetIgnore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot _config = builder.Build();
+
+            Console.WriteLine(string.Format("Version {0}", _config.GetSection("Config")["Version"]));
+
+            var app = new CommandLineApplication();
+            app.Name = "GetIgnore";
+            app.Description = @".NET Core app for quickly downloading .gitignore files.
+            Uses gitignore.io as an API";
+
+            app.HelpOption("-?|-h|--help");
+
+            app.VersionOption("-v|--version", () => {
+                return string.Format("Version {0}", _config.GetSection("Config")["Version"]);
+            });
         }
     }
 }
