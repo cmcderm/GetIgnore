d4fc6fe7dd0e459895ac5a0b1cce7da779825a79
Connor McDermott <connormcderm@gmail.com>
2019-09-13 15:08:26 -0500

    Improved on publish script

--- publish.ps1
+++ publish.ps1
@@ -11,17 +11,40 @@ dotnet publish -c release -r linux-x64
 if (-not (test-path "${env:ProgramFiles}\7-Zip\7z.exe")) {throw "${env:ProgramFiles}\7-Zip\7z.exe needed"} 
 set-alias sz "${env:ProgramFiles}\7-Zip\7z.exe"  
 
+##############
+# Windows 10 #
+##############
 $Source = ".\obj\release\netcoreapp2.2\win10-x64"
 $Target = ".\obj\release\netcoreapp2.2\GetIgnore-v" + $Version + "-Windows.zip"
 
+if (Test-Path $Target) 
+{
+  Remove-Item $Target
+}
 sz a -mx=9 $Target $Source
 
+
+#############
+#   MacOS   #
+#############
 $Source = ".\obj\release\netcoreapp2.2\osx-x64"
 $Target = ".\obj\release\netcoreapp2.2\GetIgnore-v" + $Version + "-MacOS.zip"
 
+if (Test-Path $Target) 
+{
+  Remove-Item $Target
+}
 sz a -mx=9 $Target $Source
 
+
+#############
+#   Linux   #
+#############
 $Source = ".\obj\release\netcoreapp2.2\linux-x64"
 $Target = ".\obj\release\netcoreapp2.2\GetIgnore-v" + $Version + "-Linux-x64.tar.gz"
 
+if (Test-Path $Target) 
+{
+  Remove-Item $Target
+}
 sz a -mx=9 -ttar -so archive.tar $Source | sz a -si -tgzip $Target
